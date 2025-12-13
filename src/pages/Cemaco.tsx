3718b847d2020784136ee3757488850ddb539f30
Marcos Escobar <68478662+juanmarcosescobar@users.noreply.github.com>
2025-12-12 19:03:39 -0600

    feat(regalos): add regalos page with MAX and Cemaco options

---
+++ src/pages/Cemaco.tsx
@@ -0,0 +1,9 @@
+export default function Cemaco() {
+  return (
+    <section className="wrapper w-full">
+      <div className="invite-stage mx-auto is-open">
+        {/* Contenido futuro de regalos Cemaco*/}
+      </div>
+    </section>
+  )
+}
